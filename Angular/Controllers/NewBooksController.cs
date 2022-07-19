92e8dc9db3145de19da4c46c8e66aece229dbf27
mansibhavsar22 <mansibhavsar22@gmail.com>
2022-07-19 15:09:46 +0530

    Search

--- Angular/Controllers/NewBooksController.cs
+++ Angular/Controllers/NewBooksController.cs
@@ -10,7 +10,7 @@ namespace Angular.Controllers
     public class NewBooksController : ApiController
     {
         [HttpGet]
-        public HttpResponseMessage Books(BooksModel model)
+        public HttpResponseMessage Books()
         {
             Books bookObj = new Books();
             BooksModel booksclassobj = new BooksModel();
@@ -19,6 +19,31 @@ namespace Angular.Controllers
             booksclassobj.CategoryId = bookObj.CategoryId;
             booksclassobj.PublisherId = bookObj.PublisherId;
             booksclassobj.BookName = bookObj.BookName;
+            booksclassobj.bookslist = bookObj.BooksGetList();
+            return Request.CreateResponse(HttpStatusCode.Created, booksclassobj);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Booksearch(BooksModel model)
+        {
+            Books bookObj = new Books();
+            BooksModel booksclassobj = new BooksModel();
+            booksclassobj.categorieslist = bookObj.CategoriesGetList();
+            booksclassobj.publisherslist = bookObj.PublishersGetList();
+            bookObj.CategoryId = model.CategoryId;
+            if (bookObj.CategoryId == "0") {
+                bookObj.CategoryId = null;
+            }
+            bookObj.PublisherId = model.PublisherId;
+            if (bookObj.PublisherId == "0")
+            {
+                bookObj.PublisherId = null;
+            }
+            bookObj.BookName = model.BookName;
+            if (bookObj.BookName == "0")
+            {
+                bookObj.BookName = null;
+            }
             booksclassobj.bookslist = bookObj.BooksGetList();
             return Request.CreateResponse(HttpStatusCode.Created, booksclassobj);
         }

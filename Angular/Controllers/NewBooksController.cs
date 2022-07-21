e3ad63a783f64c45b9b865c22294e183b1d361c9
mansibhavsar22 <mansibhavsar22@gmail.com>
2022-07-21 18:22:51 +0530

    Insert

--- Angular/Controllers/NewBooksController.cs
+++ Angular/Controllers/NewBooksController.cs
@@ -9,8 +9,9 @@ namespace Angular.Controllers
 {
     public class NewBooksController : ApiController
     {
+
         [HttpGet]
-        public HttpResponseMessage Books()
+        public HttpResponseMessage Books(int PageNumber)
         {
             Books bookObj = new Books();
             BooksModel booksclassobj = new BooksModel();
@@ -20,6 +21,9 @@ namespace Angular.Controllers
             booksclassobj.PublisherId = bookObj.PublisherId;
             booksclassobj.BookName = bookObj.BookName;
             booksclassobj.bookslist = bookObj.BooksGetList();
+            booksclassobj.TotalRecords = bookObj.TotalRecords;
+            booksclassobj.PageSize = bookObj.PageSize;
+            booksclassobj.PageNumber = bookObj.PageNumber;
             return Request.CreateResponse(HttpStatusCode.Created, booksclassobj);
         }
 
@@ -30,22 +34,67 @@ namespace Angular.Controllers
             BooksModel booksclassobj = new BooksModel();
             booksclassobj.categorieslist = bookObj.CategoriesGetList();
             booksclassobj.publisherslist = bookObj.PublishersGetList();
-            bookObj.CategoryId = model.CategoryId;
-            if (bookObj.CategoryId == "0") {
+            if (bookObj.CategoryId == "0")
+            {
                 bookObj.CategoryId = null;
             }
-            bookObj.PublisherId = model.PublisherId;
+            bookObj.CategoryId = model.CategoryId;
             if (bookObj.PublisherId == "0")
             {
                 bookObj.PublisherId = null;
             }
-            bookObj.BookName = model.BookName;
-            if (bookObj.BookName == "0")
+            bookObj.PublisherId = model.PublisherId;
+            if (bookObj.BookName == "")
             {
                 bookObj.BookName = null;
             }
+            bookObj.BookName = model.BookName;
+            bookObj.PageSize = model.PageSize;
+            bookObj.PageNumber = model.PageNumber;
+            booksclassobj.bookslist = bookObj.BooksGetList();
+            booksclassobj.TotalRecords = bookObj.TotalRecords;
+            booksclassobj.count = Convert.ToInt32(booksclassobj.bookslist[0].TotalRecords);
+            booksclassobj.TotalPages = (int)Math.Ceiling((double)booksclassobj.count / bookObj.PageSize);
             booksclassobj.bookslist = bookObj.BooksGetList();
             return Request.CreateResponse(HttpStatusCode.Created, booksclassobj);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage BookInsert()
+        {
+            //int BookId = id;
+            Books bookObj = new Books();
+            BooksModel booksclassobj = new BooksModel();
+            booksclassobj.categorieslist = bookObj.CategoriesGetList();
+            booksclassobj.publisherslist = bookObj.PublishersGetList();
+            bookObj.publisherslist = booksclassobj.publisherslist;
+            bookObj.categorieslist = booksclassobj.categorieslist;
+            bookObj.Load();
+            booksclassobj.BookId = bookObj.BookId;
+            booksclassobj.BookName = bookObj.BookName;
+            booksclassobj.CategoryId = bookObj.CategoryId;
+            booksclassobj.PublisherId = bookObj.PublisherId;
+            booksclassobj.IsActive = bookObj.IsActive;
+            return Request.CreateResponse(HttpStatusCode.Created, booksclassobj);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage BookInsertPost(Books bookobj)
+        {
+            Books bookObj = new Books();
+            BooksModel booksclassobj = new BooksModel();
+            booksclassobj.categorieslist = bookObj.CategoriesGetList();
+            booksclassobj.publisherslist = bookObj.PublishersGetList();
+
+            bookObj.BookId = bookobj.BookId;
+            bookObj.BookName = bookobj.BookName;
+            bookObj.CategoryId = bookobj.CategoryId;
+            bookObj.PublisherId = bookobj.PublisherId;
+            bookObj.IsActive = bookobj.IsActive;
+            bookObj.publisherslist = booksclassobj.publisherslist;
+            bookObj.categorieslist = booksclassobj.categorieslist;
+            bookObj.Save();
+            return Request.CreateResponse(HttpStatusCode.Created,1);
         }
     }
 }
--- Angular/Models/BookModel.cs
+++ Angular/Models/BookModel.cs
@@ -41,7 +41,7 @@ namespace Angular.Models
        // public List<IssueBooks> IssueBookList { get; set; }
         public int TotalRecords { get; set; }
         public int PageNumber { get; set; }
-        public int PageSize { get; set; } = 5;
+        public int PageSize { get; set; } 
         public int TotalPages { get; set; }
         public int count { get; set; }
         public int IssueId { get; set; }
